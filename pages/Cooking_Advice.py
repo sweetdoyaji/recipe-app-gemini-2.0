ad8ac62bbd1fc267578064f15da3a2cf88b14963
claire y <104372522+sweetdoyaji@users.noreply.github.com>
2025-04-18 01:13:01 +0900

    Update Cooking_Advice.py

--- pages/Cooking_Advice.py
+++ pages/Cooking_Advice.py
@@ -3,18 +3,15 @@ import streamlit as st
 # Add the code you copied from your notebook below this message
 #
 
-#
-# Here's the code to setup your session variables
-# Uncomment this block when instructed
-#
 
-# if "chat" not in st.session_state:
-#   st.session_state.chat = start_chat_session()
-# else:
-#   chat = st.session_state.chat
 
-# if "history" not in st.session_state:
-#   st.session_state.history = st.session_state.chat.history
+if "chat" not in st.session_state:
+  st.session_state.chat = start_chat_session()
+else:
+  chat = st.session_state.chat
+
+if "history" not in st.session_state:
+  st.session_state.history = st.session_state.chat.history
 
 
 
@@ -28,16 +25,16 @@ st.title("Your AI Cooking Advisor")
 # 
 
 
-# for message in st.session_state.history:
-#     with st.chat_message(message.role):
-#         st.markdown(message.parts[0].text)
+for message in st.session_state.history:
+    with st.chat_message(message.role):
+        st.markdown(message.parts[0].text)
 
-# if prompt := st.chat_input("How can I help you today?"):
+if prompt := st.chat_input("How can I help you today?"):
 
-#     with st.chat_message("user"):
-#         st.markdown(prompt)
+    with st.chat_message("user"):
+        st.markdown(prompt)
     
-#     response = chat.send_message(prompt)
+    response = chat.send_message(prompt)
 
-#     with st.chat_message("assistant"):
-#         st.markdown(response.candidates[0].content.parts[0].text)
+    with st.chat_message("assistant"):
+        st.markdown(response.candidates[0].content.parts[0].text)
